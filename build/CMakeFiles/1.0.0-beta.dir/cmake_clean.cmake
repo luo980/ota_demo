ed0860051a0ca3bebac65d2752672f325c2047a9
luo980 <luo980@gmail.com>
2023-12-19 19:12:45 +0800

    test

---
+++ build/CMakeFiles/1.0.0-beta.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1.0.0-beta"
+  "1.0.0-beta.pdb"
+  "CMakeFiles/1.0.0-beta.dir/src/camera.cpp.o"
+  "CMakeFiles/1.0.0-beta.dir/src/camera.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1.0.0-beta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/1.0.0-beta.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1.0.0-beta.
+# This may be replaced when dependencies are built.

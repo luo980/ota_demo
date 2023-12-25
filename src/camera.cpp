8f0d06706e32ef1d6630a1c45701386f19520c3a
luo980 <luo980@gmail.com>
2023-12-25 15:36:14 +0800

    test demo

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
 
 // 定义版本号或tag
-#define MAJOR_VERSION "1.0.0"
-#define MINOR_VERSION "beta"
+#define MAJOR_VERSION "1.1.0"
+#define MINOR_VERSION "alpha"
 
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         std::string arg(argv[i]);
 
-        // 检查是否有 '-v' 或 '--version' 参数
         if (arg == "-v" || arg == "--version") {
             std::cout << "version: " << MAJOR_VERSION << "-" << MINOR_VERSION << std::endl;
             return 0;
         }
     }
 
-    // 这里是程序的主要部分
-    std::cout << "Hello, World! This is a camera ability." << std::endl;
+    while(true){
+        std::cout << "Hello, World! This is a camera ability." << std::endl;
+        sleep(2);
+    }
+    // std::cout << "Hello, World! This is a camera ability." << std::endl;
 
     return 0;
 }
